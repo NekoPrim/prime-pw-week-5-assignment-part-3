a3a54fde0feafb798c5006b6e8809ef654227011
Desiree Quade <nekodez@gmail.com>
2021-11-22 22:09:30 -0600

    addToCollection +6

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,5 +1,30 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
+const collection = [];
+
+console.log(collection);
+
+function addToCollection(title, artist, yearPublished) {
+    console.log('in addToCollection');
+    let album = [title, artist, yearPublished]
+    collection.push(album);
+    return `added ${album}`;
+}
+
+console.log(addToCollection('Cups', 'Anna Kendrick', 2013));
+
+console.log(collection);
+
+console.log(addToCollection('4k', 'El Alfa', 2020));
+
+console.log(addToCollection('Makeba', 'Jain', 2017));
+
+console.log(collection);
+
+console.log(addToCollection('Bang', 'AJR', 2020));
+
+console.log(addToCollection('Burn the House Down', 'AJR', 2018));
+
+console.log(addToCollection('Sugar', 'Robin Schulz', 2015));
 
 console.log(collection);
