28d8e0c2492c9a5cecdfff63b4daf395ae2cfd91
Desiree Quade <nekodez@gmail.com>
2021-11-22 23:02:43 -0600

    working on showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,15 +1,21 @@
 console.log('***** Music Collection *****')
 
+//create a variable collections
+
 const collection = [];
 
 console.log(collection);
 
+//add a function named addToCollection
+
 function addToCollection(title, artist, yearPublished) {
     console.log('in addToCollection');
-    let album = [title, artist, yearPublished]
+    let album = [title, artist, yearPublished];
     collection.push(album);
     return `added ${album}`;
 }
+
+//add 6 albums
 
 console.log(addToCollection('Cups', 'Anna Kendrick', 2013));
 
@@ -28,3 +34,16 @@ console.log(addToCollection('Burn the House Down', 'AJR', 2018));
 console.log(addToCollection('Sugar', 'Robin Schulz', 2015));
 
 console.log(collection);
+
+//add a function named showCollection
+
+function showCollection() {
+    console.log('in showCollection');
+    for (x of collection) {
+        console.log(x);
+    }
+}
+
+//test showCollection
+
+showCollection(collection);
