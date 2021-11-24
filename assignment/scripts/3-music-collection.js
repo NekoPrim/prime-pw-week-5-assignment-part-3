440576e09213b8f4bc9bc1813abb89ad1bd35c04
Desiree Quade <nekodez@gmail.com>
2021-11-24 11:53:43 -0600

    working on required

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,14 +5,14 @@ console.log('***** Music Collection *****')
 const
  collection = [];
 
-console.log(collection);
+console.log('collection', collection);
 
 //add a function named addToCollection
 
-const addToCollection = (array, title, artist, yearPublished) => {
+const addToCollection = (object, title, artist, yearPublished) => {
     console.log('in addToCollection');
     let x = [title, artist, yearPublished];
-    array.push(x);
+    object.push(x);
     return `added: ${x}`;
 }
 
@@ -20,13 +20,13 @@ const addToCollection = (array, title, artist, yearPublished) => {
 
 console.log(addToCollection(collection, 'Cups', 'Anna Kendrick', 2013));
 
-console.log(collection);
+console.log('collection:', collection);
 
 console.log(addToCollection(collection, '4k', 'El Alfa', 2020));
 
 console.log(addToCollection(collection, 'Makeba', 'Jain', 2017));
 
-console.log(collection);
+console.log('collection:', collection);
 
 console.log(addToCollection(collection, 'Bang', 'AJR', 2020));
 
@@ -34,7 +34,7 @@ console.log(addToCollection(collection, 'Burn the House Down', 'AJR', 2018));
 
 console.log(addToCollection(collection, 'Sugar', 'Robin Schulz', 2015));
 
-console.log(collection);
+console.log('collection:', collection);
 
 //add a function named showCollection
 
@@ -42,8 +42,9 @@ const showCollection = (object) => {
     console.log('in showCollection');
     for (let x = 0; x < object.length; x ++) {
         console.log(object[x]);
-        console.log('Title:', object[x][0], '/ By artist:', object[x][1], '/ Published in year:', object[x][2]);
     }
+    return 'Title:', object[x][0], '/ By artist:', object[x][1], '/ Published in year:', object[x][2];
+
 }
 
 //test showCollection
@@ -54,17 +55,14 @@ showCollection(collection);
 
 const findByArtist = (object, artist) => {
     console.log('in findByArtist');
-    for (let x = 0; x < object.length; x ++) {
+    let x = 0;
+    for (let x in object) {
         console.log('in loop');
-        if (artist === object[x][1]) {
+        if (object[x][1] === artist) {
             console.log(`looking for ${artist}`);
-            return object[x];
-        }
-        if (artist != object[x][1] && x >= object.length) {
-            console.log(`looking for ${artist}`);
-            return `${artist} is not in this collection`;
         }
     }
+    return object[x];
 }
 
 //test findByArtist
